31d89ef91d98cddc56f019558f80171686736059
ZhangHChi <1006771855@qq.com>
2016-10-24 02:18:27 +0800

    clean

--- 4.0_JavaScriptPIC/scripts/showPic.js
+++
@@ -1,9 +0,0 @@
-function showPic(whichpic){
-	var source = whichpic.getAttribute("href");
-	var placeholder = document.getElementById("placeholder");
-	placeholder.setAttribute("src",source);
-
-	var text = whichpic.getAttribute("title");
-	var description = document.getElementById("description")
-	description.firstChild.nodeValue = text; //把description对象的第一个子节点的nodeValue属性值设置为变量text的值。
-}
